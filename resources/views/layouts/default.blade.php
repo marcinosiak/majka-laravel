81b13624088f8e164a822059e383e274536adc2a
marcinosiak <om77@wp.pl>
2018-08-09 12:48:17 +0200

    Ukończone menu i zaczęty box po sliderem

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -25,21 +25,44 @@
   </head>
 
   <body>
-
+    <div class="container-fluid">
     <header>
-      <nav>
-        {!! menu('main_menu', 'layouts.navigation') !!}
+
+      <nav class="navbar navbar-expand-lg majka-box-bg">
+              <a class="navbar-brand" href="#">tel. 501 262 980</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  &#9776;
+              </button>
+
+              {!! menu('main_menu', 'layouts.navigation') !!}
       </nav>
+
+
+      <div class="row vertical-center top-title">
+          <div class="col-sm text-center">
+            <h2>Karolina Kardasz</h2>
+            <h5>Indywidualna Praktyka Położnicza</h5>
+          </div>
+          <div class="col-sm-3 text-center">
+            <img alt="logo" src="{{ asset('images/logo.png') }}">
+          </div>
+          <div class="col-sm">
+            <h1>Szkoła Rodzenia Majka</h1>
+          </div>
+      </div>
+
     </header>
 
     @yield('content')
 
     <!-- Bootstrap core JavaScript -->
-    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     @yield('script')
 
-
+    </div> <!-- end container-fluid -->
   </body>
 
 </html>
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -18,6 +18,26 @@
     </li>
   </ul>
 
+  <div class="row majka-box majka-box-bg justify-content-md-center py-5">
+
+      <div class="col col-lg-4 text-center pt-lg-5 majka-box-item">
+        <p class="majka-box-big-title">Witam w Szkole Rodzenia Majka. Zapraszam do zapoznania się z ofertą.</p>
+      </div>
+
+      <div class="col-lg-auto majka-box-item">
+        <h6>Wypożycz TENS</h6>
+        <img alt="logo" src="{{ asset('images/tens.png') }}">
+        <p class="">Istnieje możliwość wypożyczenia TENS-u na czas porodu.</p>
+        <a href="#">Zobacz więcej</a>
+      </div>
+
+      <div class="col col-lg-3 majka-box-item">
+        <h6>Profil na FB</h6>
+        <img alt="logo" src="{{ asset('images/fb.png') }}">
+        <p>Zapraszam na profil na Facebooku</p>
+        <a href="#">Facebook</a>
+      </div>
+  </div>
 
 @endsection
 
